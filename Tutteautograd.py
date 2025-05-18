9cde63da8b2e4b28eb3d5dce5ed4669003e6ffd2
VogelFlug <dave.barac@gmx.de>
2025-05-18 22:48:07 +0200

    Added a bunch of start files

---
+++ Tutteautograd.py
@@ -0,0 +1,10 @@
+#Imports
+import numpy as np
+import torch
+from io import StringIO
+
+def main():
+    # Get datafile, fixed for now
+    # print('Enter filepath:')
+    # path = input()
+    x = "data/basicexample.txt"
