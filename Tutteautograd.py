c658207ce3cc1b69f3b09d861d78093b00296d9f
VogelFlug <dave.barac@gmx.de>
2025-05-19 23:34:07 +0200

    Implemented scuffed ass Graph file reader

--- Tutteautograd.py
+++ Tutteautograd.py
@@ -5,13 +5,18 @@ import sys
 from io import StringIO
 
 #Internal Imports
-from classes import TwoDGraph
+from classes.TwoDGraph import TwoDGraph
 
 def main(autopath: str):
     # Get datafile
     x = autopath
     print(x)
-    
+    data = ""
+    with open(x , "r") as f:
+        data = f.read()
+    Graph = TwoDGraph(data)
+    print(Graph.vertices)
+    print(Graph.faces)
 
 if __name__ == '__main__':
     main(sys.argv[1])
--- classes/TwoDGraph.py
+++ classes/TwoDGraph.py
@@ -1,9 +1,78 @@
-import numpy as np
-
 class TwoDGraph:
-    vertices: list[int,int]
-    faces: list[int,int,int]
+    vertices: tuple[int,int]
+    faces: tuple[int,int,int]
+    vertexnumber: int
+    facenumber: int
 
-    @classmethod
-    def getgraphfromvgl(data: str):
+    def __init__(self, data: str):
+        self.get2dgraphfromvgl(data)
+
+    def get2dgraphfromvgl(self, data: str):
+        lines = data.splitlines()
+        nroflines = len(lines)
+        linecounter = 0
+
+        #get rid of header
+        while(lines[linecounter][0] != "#"):
+            linecounter += 1
+        linecounter += 1
+        
+        #get number of vertices and faces
+        self.vertexnumber = lines[linecounter][0]
+        self.facenumber = lines[linecounter][2]
+        linecounter += 1
+
+        #get vertexpositions in the most scuffed way possible
+        vertices = []
+        while(lines[linecounter][0] != "#"):
+            vertex = []
+            line = lines[linecounter]
+            chacount = 0
+            linened = False
+            while(True):
+                number =""
+
+                while(line[chacount] != " "):
+                    number = number + line[chacount]
+                    if(chacount == len(line)-1):
+                        vertex.append(int(number))
+                        linened = True
+                        break
+                    chacount += 1
+                if(linened == True):
+                    break
+                chacount += 1
+                vertex.append(int(number))
+            vertices.append(vertex)
+            linecounter += 1
+        self.vertices = vertices
+        linecounter += 1
+
+        #get facelist in the same scuffed way
+        faces = []
+        while(linecounter < nroflines):
+            face = []
+            line = lines[linecounter]
+            chacount = 0
+            linened = False
+            while(True):
+                number =""
+
+                while(line[chacount] != " "):
+                    number = number + line[chacount]
+                    if(chacount == len(line)-1):
+                        face.append(int(number))
+                        linened = True
+                        break
+                    chacount += 1
+                if(linened == True):
+                    break
+                chacount += 1
+                face.append(int(number))
+            faces.append(face)
+            linecounter += 1
+        self.faces = faces
+
         return
+    
+    
