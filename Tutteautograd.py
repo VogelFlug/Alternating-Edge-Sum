c7041f4b6a7b66227f6ff403e5000d4778e5e996
VogelFlug <dave.barac@gmx.de>
2025-05-19 21:44:25 +0200

    First example completed

--- Tutteautograd.py
+++ Tutteautograd.py
@@ -1,10 +1,17 @@
 #Imports
 import numpy as np
 import torch
+import sys
 from io import StringIO
 
-def main():
-    # Get datafile, fixed for now
-    # print('Enter filepath:')
-    # path = input()
-    x = "data/basicexample.txt"
+#Internal Imports
+from classes import TwoDGraph
+
+def main(autopath: str):
+    # Get datafile
+    x = autopath
+    print(x)
+    
+
+if __name__ == '__main__':
+    main(sys.argv[1])
---
+++ classes/TwoDGraph.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class TwoDGraph:
+    vertices: list[int,int]
+    faces: list[int,int,int]
+
+    @classmethod
+    def getgraphfromvgl(data: str):
+        return
